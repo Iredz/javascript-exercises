61d4c0d5f807fb279ace9926ae779a5cdf18ad61
Oscar I. Rojas <oscarrojas991@protonmail.com>
2026-04-21 03:53:22 +0000

    Feat: Finished Exercises 06 and 07 from foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    return arr.filter((item) => !args.includes(item))
 };
+
+removeFromArray([1, 2, 3, 4], 1, 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    isYearDivisibleByFour = year % 4 === 0;
+    isCentury = year % 100 === 0;
+    isYearDivisibleByFourHundred = year % 400 === 0;
 
+    if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
