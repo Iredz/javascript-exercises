0a1fe45660520080605e74cdd94747fb1b8d550c
Oscar I. Rojas <oscarrojas991@protonmail.com>
2026-04-21 20:23:37 +0000

    Cleaned up a bit exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
 const convertToCelsius = function (fahrenheit) {
-    return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+
+    let result = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+    return result;
 };
 
 const convertToFahrenheit = function (celsius) {
-    return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+    let result = Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+    return result;
 };
 
 // Do not edit below this line
